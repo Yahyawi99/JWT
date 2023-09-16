ada84a5428094829ad4b61e396149e8863de193e
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-16 20:03:36 +0100

    setup

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+// DB
+const connectDB = require("./db/connect");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// router
+const MainRouter = require("./routes/main");
 
 // middleware
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
+
+app.use("/api/v1", MainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
+
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
